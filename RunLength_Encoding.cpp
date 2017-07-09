8a2b0f4aaab7aa55f7d50ddb7b1eeb2aad43f816
Krishna Kumar Vasudevan <connect_krishna@hotmail.com>
2017-07-09 21:57:19 +0530

    Add files via upload

---
+++ RunLength_Encoding.cpp
@@ -0,0 +1,36 @@
+
+
+#include "stdafx.h"
+#include <iostream>
+#include <sstream>
+#include <string.h>
+using namespace std;
+
+void *encode(char *src)
+{
+	int rLen;
+	int len = strlen(src);
+	int i, j = 0, k; int first;
+	std::ostringstream out;
+	for (i = 0; i < len; i++)
+	{
+		first = i;
+		rLen = 1;
+		while (i + 1 < len && src[i] == src[i + 1])
+		{
+			rLen++;
+			i++;
+		}
+		out << rLen;
+		out << src[first];
+	}
+	cout << out.str();
+
+}
+
+int main()
+{
+	char src[] = "geeeeeeeekkssssekkkkkss";
+	encode(src);
+	return 0;
+}
